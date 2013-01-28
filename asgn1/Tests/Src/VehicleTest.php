9b60b0116fd20f946983bfb38769125866ff26a7
Lisa <lisa@lisa-2.local>
2013-01-27 23:09:46 -0800

    Add a test to help get code-coverage running

---
+++ asgn1/Tests/Src/VehicleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+// Tests for NS_Vehicle
+
+class VehicleTest extends PHPUnit_Framework_TestCase
+{
+    public function test_getYear()
+    {
+        echo "this is test_getYear";
+    }
+}
+
+?>
